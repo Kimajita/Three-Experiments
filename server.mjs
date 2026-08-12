fd6c34f8c593bdedc8c4e0fb87ada6f548897cde
Juri Wende <juri.wende@proton.me>
2026-08-12 17:00:43 +0200

    modules

--- server.mjs
+++ server.mjs
@@ -1,10 +1,13 @@
 import express from 'express';
+import * as http from 'http';
+import * as fs from 'fs';
 
+/*
 const hostname = '127.0.0.1';
 const port = 8080;
 const app = express();
-app.use(express.static('./public'));
 
+app.use(express.static('./public'));
 app.listen(port, function (err) {
     if (err) {
         console.log('something happened :( ' + err);
@@ -13,3 +16,31 @@ app.listen(port, function (err) {
         console.log(`Listening at http://${hostname}:${port}`);
     }
 });
+*/
+
+const port = 3000;
+
+const server = http.createServer(function (request, response) {
+
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    fs.readFile('./public/index.html', function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('Error: File Not Found')
+        } else {
+            response.write(data);
+        }
+        response.end();
+    })
+})
+
+server.listen(port, function (error) {
+    if (error) {
+        console.log('something happened :(', error);
+    } else {
+        console.log(`Listening on http://localhost:${port}`);
+    }
+})
