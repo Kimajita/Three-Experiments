4526ca7766890c57b7db253e2c6fbd1290f5f0bd
Juri Wende <juri.wende@proton.me>
2026-08-16 15:51:10 +0200

    miau :C

--- server.mjs
+++ server.mjs
@@ -1,50 +1,11 @@
 import express from 'express';
-import * as cors from 'cors';
-
-import * as http from 'http';
-import * as fs from 'fs';
+import cors from 'cors';
 
 const host = '127.0.0.1';
 const port = 8080;
 const app = express();
 
-//app.use(express.static('./public'));
-
-
-
-app.get('/', function (req, res) {
-    res.send('hi :)');
-});
-
-app.get('/art', function (req, res) {
-    res.json([
-        { id: 1, name: 'Heart / Knot' },
-        { id: 2, name: 'Random' }
-    ])
-});
-
-app.get('/art/:id', function (req, res) {
-    const id = Number(req.params.id);
-
-    const projects = [
-        { id: 1, name: 'Heart / Knot' },
-        { id: 2, name: 'Random' }
-    ]
-
-    const chosenProject = projects.find((project) => project.id === id);
-    res.json(chosenProject);
-});
-
-app.get('/message', function (req, res) {
-    res.json({ message: "Hello from backend" });
-});
-
-
-
-
-
-
-
+app.use(express.static('./public'));
 
 app.listen(port, function (err) {
     if (err) {
