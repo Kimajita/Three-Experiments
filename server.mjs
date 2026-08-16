ea4715c0e768ff95c756c150d0bc195f5615cce6
Juri Wende <juri.wende@proton.me>
2026-08-16 14:48:10 +0200

    cors node module

--- server.mjs
+++ server.mjs
@@ -1,46 +1,56 @@
 import express from 'express';
+import * as cors from 'cors';
+
 import * as http from 'http';
 import * as fs from 'fs';
 
-/*
-const hostname = '127.0.0.1';
+const host = '127.0.0.1';
 const port = 8080;
 const app = express();
 
-app.use(express.static('./public'));
+//app.use(express.static('./public'));
+
+
+
+app.get('/', function (req, res) {
+    res.send('hi :)');
+});
+
+app.get('/art', function (req, res) {
+    res.json([
+        { id: 1, name: 'Heart / Knot' },
+        { id: 2, name: 'Random' }
+    ])
+});
+
+app.get('/art/:id', function (req, res) {
+    const id = Number(req.params.id);
+
+    const projects = [
+        { id: 1, name: 'Heart / Knot' },
+        { id: 2, name: 'Random' }
+    ]
+
+    const chosenProject = projects.find((project) => project.id === id);
+    res.json(chosenProject);
+});
+
+app.get('/message', function (req, res) {
+    res.json({ message: "Hello from backend" });
+});
+
+
+
+
+
+
+
+
 app.listen(port, function (err) {
     if (err) {
         console.log('something happened :( ' + err);
     } else {
         console.log();
-        console.log(`Listening at http://${hostname}:${port}`);
+        console.log(`Listening at http://${host}:${port}`);
     }
 });
-*/
-
-const port = 3000;
-
-const server = http.createServer(function (request, response) {
-
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-
-    fs.readFile('./public/index.html', function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('Error: File Not Found')
-        } else {
-            response.write(data);
-        }
-        response.end();
-    })
-})
-
-server.listen(port, function (error) {
-    if (error) {
-        console.log('something happened :(', error);
-    } else {
-        console.log(`Listening on http://localhost:${port}`);
-    }
-})
