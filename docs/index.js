0ceaf7ae260a01fe05a7bf4492153ab9a257537f
Juri Wende <juri.wende@proton.me>
2026-08-18 23:39:03 +0200

    arschloch blume

--- docs/index.js
+++ docs/index.js
@@ -1,10 +1,11 @@
-import * as THREE from 'three';
+import * as THREE from 'three/webgpu';
 import { OrbitControls } from 'orbitControls';
+import * as TSL from 'three/tsl';
 
 //################################################## // VARIABLES
 
 let scene, camera, light, pointLight, renderer, controls;
-let cameraDistance = 1; let fieldOfView = 67; let nearPlane = 0.1; let farPlane = 1500;
+let cameraDistance = 7; let fieldOfView = 67; let nearPlane = 0.1; let farPlane = 1500;
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -16,25 +17,28 @@ async function init() {
 
     //############################################## // LIGHTS, CAMERA, ACTION!
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000000);
 
     camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
     camera.position.z = cameraDistance;
 
-    light = new THREE.HemisphereLight(0xffffff, 0x220055, 1);
+    light = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
     scene.add(light);
 
     pointLight = new THREE.PointLight(0xffffff, 1, 0, 0);
-    //pointLight.position.z = cameraDistance / 2;
     scene.add(pointLight)
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer = new THREE.WebGPURenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(width, height);
 
     document.body.appendChild(renderer.domElement);
     controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
 
     draw();
+
+    await renderer.init();
     render();
 }
 
@@ -45,24 +49,54 @@ function render() {
     renderer.render(scene, camera);
 }
 
-//################################################## // INTERPOLATE
-const speed = 0.001;
-const t = (Math.sin(Date.now() * speed) + 1) / 2;
-function lerp(start, end, t) { return start * (1 - t) + end * t; }
-
 //################################################## // SCENE
 function draw() {
 
-    let geo = new THREE.BoxGeometry(5, 3, 7);
-    let mat = new THREE.MeshPhongMaterial({
-        color: 0x220055, side: THREE.BackSide,
-        specular: 0xaa33ff, shininess: 3,
+    const box_geo = new THREE.BoxGeometry(5, 5, 5);
+    const box_mat = new THREE.MeshPhongMaterial({
+        color: 0x000000, side: THREE.BackSide, //0x220055
+        specular: 0x555555, shininess: 3, //0xaa33ff
     });
-    const room = new THREE.Mesh(geo, mat);
-    scene.add(room);
+    const box = new THREE.Mesh(box_geo, box_mat);
+    scene.add(box);
+
+    let radius = 1, tube = 0.25, tubeSegments = 128, radialSegments = 32, p = 1, q = 5;
+    let knot_geo = new THREE.TorusKnotGeometry(radius, tube, tubeSegments, radialSegments, p, q);
+
+    //______________________________________________ // Material
+    const knot_mat = new THREE.MeshPhysicalNodeMaterial({
+        metalness: 0.0,
+        roughness: 0.0,
+        transmission: 0.5,
+        thickness: 1.0,
+        ior: 1.75,
+        transparent: true,
+        opacity: 0.42,
+        side: THREE.DoubleSide,
+    });
+
+    const col1 = TSL.uniform(new THREE.Color(0xff0089));
+    const col2 = TSL.uniform(new THREE.Color(0xc400ff));
+    const gradient = TSL.sin(TSL.positionLocal.length().mul(3.0).add(TSL.time)).mul(-0.5).add(0.5);
+    const color = TSL.mix(col1, col2, gradient);
+    knot_mat.emissiveNode = color;
+
+    knot_mat.positionNode = TSL.Fn(function() {
+        const pos = TSL.positionLocal;
+        const normal = TSL.normalLocal;
+
+        const displace = TSL.sin(TSL.time.mul(3.0).add(pos.y.mul(5.0))).mul(0.075);
+
+        return pos.add(normal.mul(displace));
+    })();
+
+    const knot = new THREE.Mesh(knot_geo, knot_mat);
+    scene.add(knot);
 
     function step() {
         requestAnimationFrame(step);
+
+        knot.geometry = new THREE.TorusKnotGeometry(radius, tube, tubeSegments, radialSegments, p, q);
 
     } step();
 }
